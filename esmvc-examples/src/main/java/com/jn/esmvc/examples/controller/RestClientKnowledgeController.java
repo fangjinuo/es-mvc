2a90f547f06bd62d263bbb93e14bc6072af0f5a1
fs1194361820@163.com <fs1194361820@163.com>
2020-07-06 06:56:09 +0800

    * rename it

--- esmvc-examples/src/main/java/com/jn/esmvc/examples/controller/RestClientKnowledgeController.java
+++ esmvc-examples/src/main/java/com/jn/esmvc/examples/controller/RestClientKnowledgeController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/https/knowledge")
-public class HttpRestClientKnowledgeController {
+public class RestClientKnowledgeController {
 
     private ESRestKnowledgeService knowledgeService;
 
