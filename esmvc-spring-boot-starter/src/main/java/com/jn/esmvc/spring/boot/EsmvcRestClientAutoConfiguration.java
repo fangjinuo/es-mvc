45e85e98a3e2ca219614af8de2931b6b59a6de32
fs1194361820@163.com <fs1194361820@163.com>
2020-09-08 10:08:22 +0800

    * 移除不必要的依赖

--- esmvc-spring-boot-starter/src/main/java/com/jn/esmvc/spring/boot/EsmvcRestClientAutoConfiguration.java
+++ esmvc-spring-boot-starter/src/main/java/com/jn/esmvc/spring/boot/EsmvcRestClientAutoConfiguration.java
@@ -6,6 +6,7 @@ import com.jn.esmvc.service.config.rest.DefaultRestClientBuilderCustomizer;
 import com.jn.esmvc.service.config.rest.EsmvcRestClientProperties;
 import com.jn.esmvc.service.config.rest.RestClientBuilderCustomizer;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
@@ -18,6 +19,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AutoConfigureAfter(ScrollContextCacheAutoConfiguration.class)
 @ConditionalOnProperty(name = "esmvc.rest.enabled", havingValue = "true", matchIfMissing = true)
@@ -44,10 +46,10 @@ public class EsmvcRestClientAutoConfiguration {
     @Autowired
     public ESRestClient esRestClient(
             @Qualifier("esmvcRestClientProperties") EsmvcRestClientProperties esmvcProperties,
-            List<RestClientBuilderCustomizer> builderCustomizers) {
+            ObjectProvider<RestClientBuilderCustomizer> builderCustomizers) {
         return new ESRestClientBuilder()
                 .properties(esmvcProperties)
-                .restClientBuilderCustomizers(builderCustomizers)
+                .restClientBuilderCustomizers(builderCustomizers.stream().collect(Collectors.toList()))
                 .build();
     }
 
