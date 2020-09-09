69bb92f784a66d15fc6cc35d61c302e3d934a5fc
fs1194361820@163.com <fs1194361820@163.com>
2020-09-09 18:29:42 +0800

    * 更新目录

--- esmvc-spring-boot-starter/src/main/java/com/jn/esmvc/spring/boot/SearchGuardRestClientAutoConfiguration.java
+++ esmvc-spring-boot-starter/src/main/java/com/jn/esmvc/spring/boot/SearchGuardRestClientAutoConfiguration.java
@@ -3,14 +3,12 @@ package com.jn.esmvc.spring.boot;
 import com.jn.esmvc.service.config.rest.EsmvcRestClientProperties;
 import com.jn.esmvc.service.security.searchguard.SearchGuardRestClientBuilderCustomizer;
 import com.jn.esmvc.service.security.searchguard.propertis.SearchGuardSSLProperties;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@AutoConfigureBefore(EsmvcRestClientAutoConfiguration.class)
 @ConditionalOnProperty(name = "esmvc.rest.primary.search-guard-ssl.enabled", havingValue = "true", matchIfMissing = false)
 @ConditionalOnBean(EsmvcRestClientProperties.class)
 @Configuration("searchGuardRestClientAutoConfiguration")
@@ -19,12 +17,12 @@ public class SearchGuardRestClientAutoConfiguration {
 
     @Bean
     @ConfigurationProperties(prefix = "esmvc.rest.primary.search-guard-ssl")
-    public SearchGuardSSLProperties searchGuardSSLProperties(){
+    public SearchGuardSSLProperties searchGuardSSLProperties() {
         return new SearchGuardSSLProperties();
     }
 
     @Bean
-    public SearchGuardRestClientBuilderCustomizer searchGuardRestClientBuilderCustomizer(SearchGuardSSLProperties searchGuardSSLProperties, EsmvcRestClientProperties esmvcRestClientProperties){
+    public SearchGuardRestClientBuilderCustomizer searchGuardRestClientBuilderCustomizer(SearchGuardSSLProperties searchGuardSSLProperties, EsmvcRestClientProperties esmvcRestClientProperties) {
         SearchGuardRestClientBuilderCustomizer searchGuardConfig = new SearchGuardRestClientBuilderCustomizer();
         searchGuardConfig.setSearchGuardSSLProperties(searchGuardSSLProperties);
         searchGuardConfig.setEsmvcRestClientProperties(esmvcRestClientProperties);
