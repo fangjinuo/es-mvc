c82d83a8b216bf8cbd0cb9237f78e89d972e0205
fs1194361820@163.com <fs1194361820@163.com>
2020-09-11 07:05:56 +0800

    * 支持默认的type

--- esmvc-model/src/main/java/com/jn/esmvc/model/Type.java
+++ esmvc-model/src/main/java/com/jn/esmvc/model/Type.java
@@ -1,10 +1,12 @@
 package com.jn.esmvc.model;
 
+import com.jn.esmvc.model.utils.ESModels;
+
 import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 public @interface Type {
-    String value() default "_doc";
+    String value() default ESModels.INDEX_DEFAULT_TYPE;
 }
--- esmvc-model/src/main/java/com/jn/esmvc/model/utils/ESModels.java
+++ esmvc-model/src/main/java/com/jn/esmvc/model/utils/ESModels.java
@@ -1,14 +1,12 @@
 package com.jn.esmvc.model.utils;
 
-import com.jn.esmvc.model.ESDocumentIdGenerator;
-import com.jn.esmvc.model.ESModelIdGenerator;
-import com.jn.esmvc.model.Index;
-import com.jn.esmvc.model.Type;
-import com.jn.esmvc.model.AbstractESModel;
+import com.jn.esmvc.model.*;
 import com.jn.langx.util.Preconditions;
 
 public class ESModels {
+    public static final String INDEX_DEFAULT_TYPE = "_doc";
     public static final ESModelIdGenerator defaultModelIdGenerator = new ESModelIdGenerator();
+
     public static String getIndex(Class<? extends AbstractESModel> modelClass) {
         try {
             Index indexAnnotation = modelClass.getAnnotation(Index.class);
@@ -21,17 +19,20 @@ public class ESModels {
     public static String getType(Class<? extends AbstractESModel> modelClass) {
         try {
             Type typeAnnotation = modelClass.getAnnotation(Type.class);
+            if (typeAnnotation == null) {
+                return INDEX_DEFAULT_TYPE;
+            }
             return typeAnnotation.value();
         } catch (Throwable ex) {
             throw new IllegalArgumentException("Can't find an annotation in class [" + modelClass.getCanonicalName() + "]");
         }
     }
 
-    public static <MODEL extends AbstractESModel>String getId(MODEL model){
+    public static <MODEL extends AbstractESModel> String getId(MODEL model) {
         return getId(model, defaultModelIdGenerator);
     }
 
-    public static <MODEL extends AbstractESModel>String getId(MODEL model, ESDocumentIdGenerator<MODEL> documentIdGenerator ){
+    public static <MODEL extends AbstractESModel> String getId(MODEL model, ESDocumentIdGenerator<MODEL> documentIdGenerator) {
         Preconditions.checkNotNull(documentIdGenerator);
         return documentIdGenerator.get(model);
     }
