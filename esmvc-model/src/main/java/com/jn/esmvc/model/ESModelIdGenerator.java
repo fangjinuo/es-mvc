724909c71bec192db3e75db11e385f17c2711714
370039454 <43571363+370039454@users.noreply.github.com>
2020-09-11 17:21:07 +0800

    增加非空判断 (#3)

--- esmvc-model/src/main/java/com/jn/esmvc/model/ESModelIdGenerator.java
+++ esmvc-model/src/main/java/com/jn/esmvc/model/ESModelIdGenerator.java
@@ -2,6 +2,7 @@ package com.jn.esmvc.model;
 
 import com.jn.esmvc.model.utils.ESModels;
 import com.jn.langx.util.Dates;
+import com.jn.langx.util.Emptys;
 import com.jn.langx.util.Strings;
 import com.jn.langx.util.id.UuidGenerator;
 
@@ -18,6 +19,9 @@ public class ESModelIdGenerator<MODEL extends AbstractESModel> implements ESDocu
 
     @Override
     public String get(MODEL model) {
+        if(Emptys.isEmpty(model)){
+            return Dates.format(new Date(), PATTERN) + "_" + uuidGenerator.get();
+        }
         String id = model.get_id();
         if (Strings.isBlank(id)) {
             String index = ESModels.getIndex(model.getClass());
