8e468281090d06d9522568358a3d861d4458f95d
admin <370039454@qq.com>
2020-09-11 16:53:04 +0800

    增加非空判断

--- esmvc-service/src/main/java/com/jn/esmvc/service/request/document/action/count/RestCountRequestAdapter.java
+++ esmvc-service/src/main/java/com/jn/esmvc/service/request/document/action/count/RestCountRequestAdapter.java
@@ -1,6 +1,7 @@
 package com.jn.esmvc.service.request.document.action.count;
 
 import com.jn.esmvc.service.request.document.action.RequestAdapter;
+import com.jn.langx.util.Emptys;
 import org.elasticsearch.client.core.CountRequest;
 
 public class RestCountRequestAdapter implements RequestAdapter<com.jn.esmvc.service.request.document.action.count.CountRequest,CountRequest> {
@@ -11,7 +12,9 @@ public class RestCountRequestAdapter implements RequestAdapter<com.jn.esmvc.serv
         to.types(from.types());
         to.indicesOptions(from.indicesOptions());
         to.preference(from.preference());
-        to.minScore(from.minScore());
+        if(Emptys.isNotEmpty(from.minScore())){
+            to.minScore(from.minScore());
+        }
         to.routing(from.routing());
         to.source(from.source());
         to.terminateAfter(from.terminateAfter());
